5b422c1d1dd660d928d831e551309bcc20d72cb4
Eduardo Maturana <edu.maturana.c@gmail.com>
2022-11-13 23:19:38 -0300

    feat: add docker compose to raise database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ToursModule } from './tours/tours.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), ToursModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT) || 3306,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+    }),
+    ToursModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/tours/tours.module.ts
+++ src/tours/tours.module.ts
@@ -5,19 +5,7 @@ import { ToursService } from './tours.service';
 import { ToursEntity } from './tours.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-      entities: [ToursEntity],
-      synchronize: true,
-    }),
-    TypeOrmModule.forFeature([ToursEntity]),
-  ],
+  imports: [TypeOrmModule.forFeature([ToursEntity])],
   providers: [ToursService],
   controllers: [ToursController],
 })
