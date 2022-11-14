fb4bef8aecb03fa3888b286c49f1704addcee30f
Eduardo Maturana <edu.maturana.c@gmail.com>
2022-11-13 22:14:34 -0300

    chore: set tours module correctly and remove duplicate sentences

--- src/tours/tours.module.ts
+++ src/tours/tours.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { ToursController } from './tours.controller';
-// import { ToursService } from './tours.service';
-import { ToursEntity } from './tours.entity';
-import { ToursService } from './tours.service';
 import { ToursController } from './tours.controller';
+import { ToursService } from './tours.service';
+import { ToursEntity } from './tours.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ToursEntity])],
   providers: [ToursService],
   controllers: [ToursController],
-  //   controllers: [UsersController],
-  //   providers: [UsersService],
 })
-export class UsersModule {}
+export class ToursModule {}
